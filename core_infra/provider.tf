e7b270397e0d162cf2aae1fe5cc8019c9e620f98
Shuaib <shuaibh97@gmail.com>
2023-11-30 19:34:40 +0000

    adding api as secret

--- core_infra/provider.tf
+++ core_infra/provider.tf
@@ -12,7 +12,7 @@ terraform {
       version = "5.26.0"
     }
     mongodbatlas = {
-      source  = "mongodb/mongodbatlas",
+      source = "mongodb/mongodbatlas",
     }
   }
 }
@@ -24,8 +24,8 @@ provider "aws" {
 }
 
 provider "mongodbatlas" {
-  public_key = jsondecode(data.aws_secretsmanager_secret_version.secret-version.secret_string)["mongodbatlas_public_key"]
-  private_key  = jsondecode(data.aws_secretsmanager_secret_version.secret-version.secret_string)["mongodbatlas_private_key"]
+  public_key  = jsondecode(data.aws_secretsmanager_secret_version.secret-version.secret_string)["mongodbatlas_public_key"]
+  private_key = jsondecode(data.aws_secretsmanager_secret_version.secret-version.secret_string)["mongodbatlas_private_key"]
 }
 
 
