125a75a47a47d8b06f94e1f44dec98ca784c6cd9
Shuaib <shuaibh97@gmail.com>
2023-11-30 21:14:40 +0000

    adding api as secret

--- iam/iam.tf
+++ iam/iam.tf
@@ -2,9 +2,9 @@ data "aws_iam_role" "iam_role" {
   name = "github-actions-role"
 }
 
-# resource "aws_iam_role_policy_attachment" "policy_attachment" {
-#   role       = data.aws_iam_role.iam_role.name
-#   policy_arn = aws_iam_policy.gihub_actions.arn
-# }
+resource "aws_iam_role_policy_attachment" "policy_attachment" {
+  role       = data.aws_iam_role.iam_role.name
+  policy_arn = aws_iam_policy.gihub_actions.arn
+}
 
 
--- iam/policy.tf
+++ iam/policy.tf
@@ -20,7 +20,7 @@ data "aws_iam_policy_document" "gihub_actions" {
   }
 }
 
-# resource "aws_iam_policy" "gihub_actions" {
-#   name   = "github_actions_policy"
-#   policy = data.aws_iam_policy_document.gihub_actions.json
-# }
+resource "aws_iam_policy" "gihub_actions" {
+  name   = "github_actions_policy"
+  policy = data.aws_iam_policy_document.gihub_actions.json
+}
