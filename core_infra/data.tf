dbeb2e3d915f2da58533b31a18a927f15cdf35f1
Shuaib <shuaibh97@gmail.com>
2023-11-30 19:31:38 +0000

    adding api as secret

--- core_infra/data.tf
+++ core_infra/data.tf
@@ -1,7 +1,7 @@
 data "aws_secretsmanager_secret_version" "secret-version" {
-  secret_id = data.aws_secretsmanager_secret.example.id
+  secret_id = data.aws_secretsmanager_secret.secret.id
 }
 
-data "aws_secretsmanager_secret" "by-name" {
+data "aws_secretsmanager_secret" "secret" {
   name = "mongodb-atlas-api-key"
 }
