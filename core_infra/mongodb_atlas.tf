67f94b180070dfcef54f5a3255d6de0d0b5d981f
Shuaib <shuaibh97@gmail.com>
2023-12-07 19:24:08 +0000

    adding mongodb module to infra

--- core_infra/mongodb_atlas.tf
+++ core_infra/mongodb_atlas.tf
@@ -1,6 +1,6 @@
 module "mongodbatlas" {
   source = "../modules/mongodbatlas"
   cluster_name = "tf-mongodb-example"
-  project_id = "656609c4b1d783082c02d45b"
+  project_id = var.project_id
   provider_name = "AWS"
 }
